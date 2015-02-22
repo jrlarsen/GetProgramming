3c1260d8c05db0ba4bf69f9e27fffdfa4fd72fa2
John Larsen <jr.larsen@mac.com>
2015-02-22 14:37:28 +0000

    Listing 3.4 - player object

---
+++ Chapter 3 - Players/listing3.4.js
@@ -0,0 +1,21 @@
+// Adventures In Code
+// Listing 3.4
+
+var player1 = {};
+
+player1.name = "Kandra";
+player1.location = "The Armoury";
+
+console.log(player1.name + " is in " + player1.location);
+
+
+/**
+ * Adventures
+ *
+ * 1) Add another property to player1.
+ *    Include the new property in the string logged to the console.
+ *
+ * 2) Add a second player using an object.
+ *    Log their details to the console.
+ *
+ */
