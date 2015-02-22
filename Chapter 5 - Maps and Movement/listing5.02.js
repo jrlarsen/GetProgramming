467a46c8c783528cfa4869a65239d1afa407fe88
John Larsen <jr.larsen@mac.com>
2015-02-22 20:42:47 +0000

    Listing 5.02 - addExit method

---
+++ Chapter 5 - Maps and Movement/listing5.02.js
@@ -0,0 +1,31 @@
+// Adventures In Code
+// Listing 5.02
+// http://jsbin.com/gugami/edit?js,console
+
+function Location(title, description) {
+    this.title = title;
+    this.description = description;
+    this.exits = {};
+}
+
+Location.prototype.addExit = function(title, destination) {
+    this.exits[title] = destination;
+};
+
+var location1 = new Location("The Kitchen", "You are in a large kitchen. There is a strong stench of decay.");
+
+var location2 = new Location("The Library", "You are in a dusty, old library. Ancient books line every wall.");
+
+location1.addExit("north", location2);
+
+console.log(location1);
+
+
+/**
+ * Adventures
+ *
+ * 1) Add an exit for location2 that points back to location1.
+ *
+ * 2) Log location2 to the console.
+ *
+ */
