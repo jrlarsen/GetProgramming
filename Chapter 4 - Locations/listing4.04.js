ad9202636e50de55349ab886a55b1b60e4bb9776
John Larsen <jr.larsen@mac.com>
2015-02-22 17:27:59 +0000

    Listing 4.04 - Location log method

---
+++ Chapter 4 - Locations/listing4.04.js
@@ -0,0 +1,26 @@
+// Adventures In Code
+// Listing 4.04
+
+function Location(title, description) {
+    this.title = title;
+    this.description = description;
+
+    this.log = function() {
+        console.log(this.title + "...");
+        console.log(this.description);
+    };
+}
+
+var location1 = new Location("The Library", "You are in a dusty library. Old books line every wall.");
+
+location1.log();
+
+
+/**
+ * Adventures
+ *
+ * 1) Change the title and description used to create location1.
+ *
+ * 2) Create a second location and log its properties to the console.
+ *
+ */
