5ed88c9149fa80fcd0071921856177c2c5aa8c89
John Larsen <jr.larsen@mac.com>
2015-02-22 13:34:19 +0000

    Store a value in the playerName variable

---
+++ Chapter 3 - Players/listing3.1.js
@@ -0,0 +1,22 @@
+// We can use variables to store information about the current player.
+
+
+var playerName = "Simon";
+
+
+// The 'var' keyword is used to create a variable, a label used for a value we want to store and retrieve in our program.
+
+// We have chosen to call the variable 'playerName'. This is just a label. We chose a name that describes what the variable is for.
+
+// = "Simon"
+// We set our variable to the value "Simon".
+
+// The semicolon, ';', is used to finish our instruction.
+
+
+console.log(playerName);
+
+
+// console.log will display information on the console.
+
+// In this case, the value of the variable 'playerName' is shown on the console.
