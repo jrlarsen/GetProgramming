212aaab8623dd4adb142211b4b89353cae115217
John Larsen <jr.larsen@mac.com>
2015-02-22 17:23:42 +0000

    Listing 4.03 - Location constructor

---
+++ Chapter 4 - Locations/listing4.03.js
@@ -0,0 +1,23 @@
+// Adventures In Code
+// Listing 4.03
+// http://jsbin.com/yoluyi/edit?js,console
+
+function Location(title, description) {
+    this.title = title;
+    this.description = description;
+}
+
+var location1 = new Location("The Library", "You are in a dusty library. Old books line every wall.");
+
+console.log(location1.title);
+console.log(location1.description);
+
+
+/**
+ * Adventures
+ *
+ * 1) Change the title and description used to create location1.
+ *
+ * 2) Create a second location and log its properties to the console.
+ *
+ */
