698cef8ce383ef85becd7a4c455de2d7dd8c88c3
John Larsen <jr.larsen@mac.com>
2015-02-22 14:25:26 +0000

    Listing 3.3 - multiple players

---
+++ Chapter 3 - Players/listing3.3.js
@@ -0,0 +1,23 @@
+// Adventures In Code
+// Listing 3.3
+
+var player1Name = "Kandra";
+var player1Location = "The Armoury";
+
+var player2Name = "Dax";
+var player2Location = "The Kitchen";
+
+console.log(player1Name + " is in " + player1Location);
+console.log(player2Name + " is in " + player2Location);
+
+
+/**
+ * Adventures
+ *
+ * 1) Change the names and locations and
+ *    run the program again.
+ *
+ * 2) Add a third player.
+ *    Display their name and location in the same way.
+ *
+ */
