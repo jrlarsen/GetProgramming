86e107535d0d1996699718532ca17f2970eaada0
John Larsen <jr.larsen@mac.com>
2015-02-22 13:58:37 +0000

    Work with two variables.

---
+++ Chapter 3 - Players/listing3.2.js
@@ -0,0 +1,20 @@
+// Adventures In Code
+// Listing 3.2
+
+var playerName = "Dax";
+var playerLocation = "The Library";
+
+console.log(playerName + " is in " + playerLocation);
+
+
+/**
+ * Adventures
+ *
+ * 1) Change the values of the
+ *    playerName and playerLocation variables
+ *    and run the program again.
+ *
+ * 2) Add a new variable and include its value
+ *    in the message.
+ *
+ */
