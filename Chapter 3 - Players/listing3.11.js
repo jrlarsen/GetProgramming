6a458f96a7e35f34723469073e385db7cf9b59f1
John Larsen <jr.larsen@mac.com>
2015-02-22 16:20:00 +0000

    Listing 3.11 - show items only if there are any

---
+++ Chapter 3 - Players/listing3.11.js
@@ -0,0 +1,37 @@
+// Adventures In Code
+// Listing 3.11
+
+function log(player) {
+    console.log(player.name + " is in " + player.location);
+
+    if (player.items.length > 0) {
+        console.log(player.name + " has: " + player.items.join(', ') + '.');
+    }
+}
+
+var player1 = {
+    name : "Brin",
+    location : "The Library",
+    items : [ "a sword" , "a lamp" ]
+};
+
+var player2 = {
+    name : "Dax",
+    location : "The Kitchen",
+    items : []
+};
+
+log(player1);
+log(player2);
+
+
+/**
+ * Adventures
+ *
+ * 1) Change the items for the two players.
+ *
+ * 2) Change the comma in the join function to " and ".
+ *
+ * 3) What happens if a player has no items property at all?
+ *
+ */
