75619128dea1d229435c0b9ff0d35a3a7f248257
John Larsen <jr.larsen@mac.com>
2015-02-22 16:46:01 +0000

    Listing 4.01 - location object

---
+++ Chapter 4 - Locations/listing4.01.js
@@ -0,0 +1,22 @@
+// Adventures In Code
+// Listing 4.01
+
+var location1 = {
+    title: "The Library",
+    description: "You are in a dusty library. Old books line every wall.",
+    items: [ "an ancient key" ],
+    exits: [ "north", "east" ]
+};
+
+console.log(location1);
+
+
+/**
+ * Adventures
+ *
+ * 1) Notice how the location has been displayed on the console.
+ *
+ * 2) Add a second location using the same format.
+ *    Log it to the console.
+ *
+ */
