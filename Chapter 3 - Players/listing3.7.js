2711db1ddfc392f3e5acc954031b0dd403adc5c1
John Larsen <jr.larsen@mac.com>
2015-02-22 14:59:07 +0000

    Listing 3.7 - logging multiple players

---
+++ Chapter 3 - Players/listing3.7.js
@@ -0,0 +1,26 @@
+// Adventures In Code
+// Listing 3.7
+
+function log(player) {
+    console.log(player.name + " is in " + player.location);
+}
+
+var player1 = { name : "Kandra", location : "The Armoury" };
+var player2 = { name : "Dax", location : "The Kitchen" };
+var player3 = { name : "Brin", location : "The Library" };
+
+log(player1);
+log(player2);
+log(player3);
+
+
+/**
+ * Adventures
+ *
+ * 1) Change some names and locations.
+ *    Run the program again.
+ *
+ * 2) Add two more players.
+ *    Display their details on the console.
+ *
+ */
