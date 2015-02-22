0a1fa2bf26b0c401b2d29a806cf6d4473ad7b3ce
John Larsen <jr.larsen@mac.com>
2015-02-22 14:52:42 +0000

    Listing 3.6 - log function

---
+++ Chapter 3 - Players/listing3.6.js
@@ -0,0 +1,24 @@
+// Adventures In Code
+// Listing 3.6
+
+function log(player) {
+    console.log(player.name + " is in " + player.location);
+}
+
+var player1 = {};
+player1.name = "Kandra";
+player1.location = "The Armoury";
+
+log(player1);
+
+
+/**
+ * Adventures
+ *
+ * 1) Change the name and location of player1.
+ *    Run the program again.
+ *
+ * 2) Add another player.
+ *    Use the log function to display their details.
+ *
+ */
