f3294c395a9352c9dce3d4a74abb1e477300962e
John Larsen <jr.larsen@mac.com>
2015-02-23 22:57:09 +0000

    Wrap Player and Location constructors in functions

--- Chapter 6 - Items and Inventory/Player.js
+++ Chapter 6 - Items and Inventory/Player.js
@@ -1,37 +1,43 @@
-function Player(name) {
-    this.name = name;
-    this.items = [];
-}
+(function(aic) {
 
-Player.prototype = {
-    addItem: function(itemName) {
-        if (this.items.indexOf(itemName) === -1) {
-            this.items.push(itemName);
-        }
-    },
-
-    removeItem: function(itemName) {
-        var itemIndex = this.items.indexOf(itemName);
-        if (itemIndex !== -1) {
-            this.items.splice(itemIndex, 1);
-        }
-    },
-
-    logItems: function() {
-        var i, n;
-
-        if (this.items.length) {
-            console.log(this.name + " has:");
-            for (i = 0, n = this.items.length; i < n; i++) {
-                console.log("(" + i + ") " + this.items[i]);
-            }
-        } else {
-            console.log(this.name + " has no items.");
-        }
-    },
-
-    log: function() {
-        console.log(this.name);
-        this.logItems();
+    function Player(name) {
+        this.name = name;
+        this.items = [];
     }
-};
+
+    Player.prototype = {
+        addItem: function (itemName) {
+            if (this.items.indexOf(itemName) === -1) {
+                this.items.push(itemName);
+            }
+        },
+
+        removeItem: function (itemName) {
+            var itemIndex = this.items.indexOf(itemName);
+            if (itemIndex !== -1) {
+                this.items.splice(itemIndex, 1);
+            }
+        },
+
+        logItems: function () {
+            var i, n;
+
+            if (this.items.length) {
+                console.log(this.name + " has:");
+                for (i = 0, n = this.items.length; i < n; i++) {
+                    console.log("(" + i + ") " + this.items[i]);
+                }
+            } else {
+                console.log(this.name + " has no items.");
+            }
+        },
+
+        log: function () {
+            console.log(this.name);
+            this.logItems();
+        }
+    };
+
+    aic.Player = Player;
+
+})(AdventuresInCode);
