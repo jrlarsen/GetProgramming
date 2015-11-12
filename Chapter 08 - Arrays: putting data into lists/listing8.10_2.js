6c9c8305428f17b4edc3bcbc96ef56cac5547019
John Larsen <Jr.larsen@mac.com>
2015-11-12 17:10:26 +0000

    Get Programming

---
+++ Chapter 08 - Arrays: putting data into lists/listing8.10_2.js
@@ -0,0 +1,18 @@
+/* Get Programming with JavaScript
+ * Listing 8.10
+ * Using the arguments passed by forEach - compact
+ */
+
+[ "Dax", "Jahver", "Kandra" ].forEach(function (item, index, wholeArray) {
+  console.log("Item: " + item);
+  console.log("Index: " + index);
+  console.log("Array: " + wholeArray);
+});
+
+
+
+/* Further Adventures
+ *
+ * 1)
+ *
+ */
