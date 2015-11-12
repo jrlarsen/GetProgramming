26f3c4a9453c6c609fea6180fb85ee1cc629b398
John Larsen <Jr.larsen@mac.com>
2015-11-12 10:00:28 +0000

    Get Programming

--- Chapter 05 - Arguments: passing data to functions/listing5.09.js
+++ Chapter 05 - Arguments: passing data to functions/listing5.09.js
@@ -1,4 +1,4 @@
-/* Adventures in JavaScript
+/* Get Programming with JavaScript
  * Listing 5.09
  * Displaying a player's health
  */
@@ -18,11 +18,22 @@ showPlayerHealth("Dax", 40);
  *
  * 1) Change the showPlayerHealth function
  *    so it shows information of the form:
- *    KANDRA: health 50.
+ *    Kandra: health 50.
  *
- * 2) Change the function so that it uses
- *    a variable called healthInfo that
- *    is the only thing logged to the console.
- *    i.e. console.log(healthInfo);   
+ * 2) Call the showPlayerHealth function
+ *    using your own arguments.
+ *
+ * 3) Declare a variable called healthInfo
+ *    inside the showPlayerHealth function.
+ *
+ *    var healthInfo;
+ *
+ * 4) Assign healthInfo the string that
+ *    will be displayed.
+ *
+ *    healthInfo = playerName + ": health " + playerHealth;
+ *
+ * 5) Change the call to console.log so that
+ *    it uses the healthInfo variable.
  *
  */
