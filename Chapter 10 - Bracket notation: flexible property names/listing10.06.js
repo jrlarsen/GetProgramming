dc5259b4effd1e765efd1823c0ea5bf79436ba76
John Larsen <Jr.larsen@mac.com>
2015-11-12 19:14:14 +0000

    Get Programming

--- Chapter 10 - Bracket notation: flexible property names/listing10.06.js
+++ Chapter 10 - Bracket notation: flexible property names/listing10.06.js
@@ -1,25 +1,25 @@
-/* Adventures in JavaScript
+/* Get Programming with JavaScript
  * Listing 10.06
  * Counting words from tweets
  */
 
 var tweets = [
-    "Education is showing business the way by using technology to share information. How do we so so safely?",
-    "Enjoy a free muffin & coffee with Post Plus, our new loyalty club exclusive to subscribers!",
-    "We're LIVE on Periscope right now answering all your #pet questions - tweet us yours now!",
-    "Saw an unfinished print of M:I5 yesterday and it is my great pleasure to report it is rather splendid.",
-    "Downloading the beta of OS X El Capitan. This is going to gooch my mac for sure, but it will probably look pretty",
-    "Mud pies and muscle: H.G. Wells weighs in on #STEMeducation in an automated age",
-    "UK Space Agency brings astronaut’s mission to children across the UK #ESA #ESERO",
-    "Trying to find an old tweet that I didn't favourite at the time is…a fools game. #continuesDigging",
-    "A robot has just passed a classic test of self-awareness. What does that mean?",
-    "It was a great event. Very important to learn how to communicate about science and engineering effectively"
+  "Education is showing business the way by using technology to share information. How do we so so safely?",
+  "Enjoy a free muffin & coffee with Post Plus, our new loyalty club exclusive to subscribers!",
+  "We're LIVE on Periscope right now answering all your #pet questions - tweet us yours now!",
+  "Saw an unfinished print of M:I5 yesterday and it is my great pleasure to report it is rather splendid.",
+  "Downloading the beta of OS X El Capitan. This is going to gooch my mac for sure, but it will probably look pretty",
+  "Mud pies and muscle: H.G. Wells weighs in on #STEMeducation in an automated age",
+  "UK Space Agency brings astronaut’s mission to children across the UK #ESA #ESERO",
+  "Trying to find an old tweet that I didn't favourite at the time is…a fools game. #continuesDigging",
+  "A robot has just passed a classic test of self-awareness. What does that mean?",
+  "It was a great event. Very important to learn how to communicate about science and engineering effectively"
 ];
 
 var words = {};
 var tweetText = tweets.join(" ");
 var tweetWords = tweetText.split(" ");
-
+  
 tweetWords.forEach(function (word) {
     words[word] = 0;
 });
