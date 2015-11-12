e45422fba4a07b69bbc6446650c4617becb4c705
John Larsen <Jr.larsen@mac.com>
2015-11-12 19:05:38 +0000

    Get Programming

--- Chapter 10 - Bracket notation: flexible property names/listing10.01.js
+++ Chapter 10 - Bracket notation: flexible property names/listing10.01.js
@@ -1,4 +1,4 @@
-/* Adventures in JavaScript
+/* Get Programming with JavaScript
  * Listing 10.01
  * Bracket notation for object properties
  */
@@ -11,8 +11,14 @@ ages["Dax"] = 21;
 console.log(ages.Kandra);
 console.log(ages.Dax);
 
+
+
 /* Further Adventures
  *
- * 1)
+ * 1) Add a couple more properties to
+ *    the ages object.
+ *
+ * 2) Change the calls to console.log
+ *    to use square bracket notation as well.
  *
  */
