f52f53bdb884a1330250b153d6bf983166bcfe4a
John Larsen <jl@room51.co.uk>
2015-12-11 20:45:26 +0000

    Ch 18 first listing

---
+++ Chapter 18 - Templates: replacing placeholders with data/listing18.01.js
@@ -0,0 +1,31 @@
+/* Get Programming with JavaScript
+ * Listing 18.01
+ * Replacing one string with another
+ */
+
+var before = "Hello to Jason";
+
+var after = before.replace("Jason", "Stephen");
+
+console.log(before);
+console.log(after);
+
+
+
+/* Further Adventures
+ *
+ * 1) Run the program
+ *
+ * 2) Before the calls to console.log,
+ *    add a line of code to replace
+ *    "Hello" with "Goodbye"
+ *
+ * 3) Write a function that takes
+ *    a string and returns a new
+ *    string with the first occurence
+ *    of "Jason" replaced by "Stephen"
+ *
+ *    e.g. jasonToStephen("Hello to Jason")
+ *    should return "Hello to Stephen"
+ *
+ */
