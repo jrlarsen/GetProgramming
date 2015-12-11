2b0ca2bbc3191b2ec65318955086d22bd60842e9
John Larsen <jl@room51.co.uk>
2015-12-11 21:40:16 +0000

    New listing

---
+++ Chapter 18 - Templates: replacing placeholders with data/listing18.04.js
@@ -0,0 +1,35 @@
+/* Get Programming with JavaScript
+ * Listing 18.04
+ * Using a while loop to count
+ */
+
+var count = 1;
+
+while (count < 11) {
+    console.log(count);
+    count++;
+}
+
+
+
+/* Further Adventures
+ *
+ * 1) Run the program.
+ *
+ * 2) Change the program so it
+ *    counts down from 10 to 1.
+ *
+ * 3) Change it so it shows all
+ *    the odd numbers from 1 to 100.
+ *
+ * 4) Create an array of numbers and
+ *    assign it to a variable.
+ *
+ *    e.g. var nums = [2, 7, 1, 9];
+ *
+ * 5) Use a while loop to display
+ *    the elements of the array.
+ *
+ *    Hint: console.log(nums[count])
+ *
+ */
