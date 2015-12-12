e5c9f4374ef4cb7589d588adc4d6dbb8d652a54c
John Larsen <jl@room51.co.uk>
2015-12-12 19:04:31 +0000

    New listing

---
+++ Chapter 15 - Updates: making changes without breaking existing code/theCrypt.js
@@ -0,0 +1,21 @@
+/* Get Programming with JavaScript
+ * Chapter 15
+ * Running the game with challenges
+ */
+
+theCrypt.game.init(theCrypt.mapData, "Dax");
+
+
+
+/* Further Adventures
+ *
+ * 1) Run the program.
+ *
+ * 2) Play the game.
+ *
+ * 3) Try to break the game when playing.
+ *    Go in non-existent directions.
+ *    Pick up items that aren't there.
+ *    Use items you don't have.
+ *
+ */
