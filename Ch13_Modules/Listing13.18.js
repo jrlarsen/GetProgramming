18276179d23f8f9511a7e3a1316068bebb6d01db
John Larsen <Jr.larsen@mac.com>
2016-07-27 10:54:47 +0100

    Importing modules for The Crypt

---
+++ Ch13_Modules/Listing13.18.js
@@ -0,0 +1,17 @@
+/* Get Programming with JavaScript
+ * Listings 13.17 and 13.18
+ * Importing modules for The Crypt
+ */
+
+var game = theCrypt.getGame();
+
+
+
+/* Further Adventures
+ *
+ * 1) Run the program and play the game.
+ *
+ *    > game.get()
+ *    > game.go("east")
+ *
+ */
