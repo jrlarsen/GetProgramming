a9ce3285c66b5dc202e596903b7d25893302b4cf
John Larsen <Jr.larsen@mac.com>
2016-07-27 15:31:43 +0100

    The map builder

--- Ch14_Models/listing14.06.js
+++ Ch14_Models/listing14.06.js
@@ -1,83 +1,60 @@
 /* Get Programming with JavaScript
  * Listing 14.06
- * A game controller
+ * The map builder
  */
 
 (function () {
-
+  var buildMap = function (mapData) {  
+    var placesStore = {};
+  
+    var buildPlace = function (placeData) {
+      var place = new theCrypt.Place(placeData.title, placeData.description);
+    
+      if (placeData.items !== undefined) {
+        placeData.items.forEach(place.addItem);
+      }
+      
+      placesStore[placeData.title] = place;
+    };
+  
+    var buildExits = function (placeData) {
+      var here = placesStore[placeData.title];
+    
+      if (placeData.exits !== undefined) {
+        placeData.exits.forEach(function (exit) {
+          var there = placesStore[exit.to];
+          here.addExit(exit.direction, there);
+          here.addChallenge(exit.direction, exit.challenge);                            
+        });
+      }    
+    };
+      
+    // create all places
+    mapData.places.forEach(buildPlace);
+    
+    // link places together
+    mapData.places.forEach(buildExits);
+    
+    return placesStore[mapData.firstPlace];  
+  };
+  
   if (window.theCrypt === undefined) {
     window.theCrypt = {};
   }
-
-  theCrypt.game = (function () {
-    var player;
   
-    var renderPlayer = function () {
-      theCrypt.playerView.render(player);
-    };
+  theCrypt.buildMap = buildMap;
   
-    var renderPlace = function () {
-      theCrypt.placeView.render(player.getPlace());
-    };
-
-    var renderMessage = function (message) {
-      theCrypt.messageView.render(message);
-    };
-  
-    var init = function (mapData, playerName) {
-      var firstPlace = theCrypt.buildMap(mapData);
-    
-      player = new theCrypt.Player(playerName, 50);
-      player.addItem("The Sword of Doom");
-      player.setPlace(firstPlace);
-    
-      renderPlayer();
-      renderPlace();
-    };
-  
-    var go = function (direction) {
-      var place = player.getPlace();
-      var destination = place.getExit(direction);
-    
-      if (destination !== undefined) {
-        player.setPlace(destination);
-        renderPlace();
-      } else {
-        renderMessage("There is no exit in that direction");
-      }
-    };
-    
-    var get = function (item) {
-      var place = player.getPlace();
-      if (place.hasItem(item)) {
-        place.removeItem(item);
-        player.addItem(item);
-      } else {
-        renderMessage("You can't get that!");
-      }
-    };
-  
-    // Return the public interface
-    return {
-      me: renderPlayer,
-      here: renderPlace,
-      go: go,
-      get: get,
-      init: init
-    };
-  
-  })();
-
 })();
 
 
 /* Further Adventures
  *
- * The game controller uses the other
- * modules created for The Crypt in
- * Chapters 13 and 14.
+ * The buildMap function uses
+ * the Place constructor and needs
+ * some map data to work on.
  *
- * The controller is tested in
- * listings 14.07 and 14.08.
+ * Listing 14.08 shows a working
+ * example that imports all the
+ * modules needed.
  *
  */
