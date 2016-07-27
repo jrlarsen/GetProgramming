4c4a8f97ec3e24fe3f3363f41d5f862c7384660a
John Larsen <Jr.larsen@mac.com>
2016-07-27 15:00:14 +0100

    The User constructor

--- Ch14_Models/listing14.01.js
+++ Ch14_Models/listing14.01.js
@@ -1,72 +1,41 @@
-/* Get Programming with JavaScript
- * Listing 14.01
- * A simplified Player constructor
- */
+// Get Programming with JavaScript
+// Listing 14.01
+// The User constructor
 
-(function () {
-  if (window.theCrypt === undefined) {
-    window.theCrypt = {};
-  }
+var User = function (name) {
+    var sessions = [];
+    var totalDuration = 0;
 
-  theCrypt.Player = function (name, health) {
-    var items = [];
-    var place = null;
-
-    this.addItem = function (item) {
-      items.push(item);
+    this.addSession = function (sessionDate, duration) {
+        sessions.push({
+            "sessionDate" : sessionDate,
+            "duration"    : duration
+        });
+        totalDuration += duration;
+        return totalDuration;
     };
   
-    this.setPlace = function (destination) {
-      place = destination;
-    };
-    
-    this.getPlace = function () {
-      return place;
-    };
-
     this.getData = function () {
-      var data = {
-        "name" : name,
-        "health" : health,
-        "items" : items.slice()
-      };
-    
-      if (place !== null) {
-        data.place = place.title;
-      }
-    
-      return data;
+        return {
+            "name"    : name,
+            "total"   : totalDuration,
+            "sessions": sessions.slice()
+        };
     };
-  };
-})();
-  
+};
+
 
 
 /* Further Adventures
  *
  * 1) Run the program.
  *
- * 2) At the console prompt, enter the
- *    following statements:
+ * 2) Test the constructor at the console prompt:
  *
- *    > var p1 = new theCrypt.Player("Bob", 20)
- *    > p1.getData()
- *
- * data about the player should be displayed.
- *
- * 3) Add some items to the player.
- *
- *    > p1.addItem("a horse")
- *    > p1.addItem("a cow")
- *    > p1.getData()
- *
- * The Place constructor isn't available
- * but we can use an object with a title
- * for now.
- *
- * 4) Set a place for the player.
- *
- *    > p1.setPlace({title : "The Farm"})
- *    > p1.getData()
+ *    > var user = new User("Mahesha")
+ *    > user.addSession("2017-02-05", 120)
+ *    > user.addSession("2017-02-06", 35)
+ *    > user.getData()
+ *    > user.getData().total
  *
  */
