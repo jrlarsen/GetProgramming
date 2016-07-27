637164ec22199a490f3b2210997ec77593f05f85
John Larsen <Jr.larsen@mac.com>
2016-07-27 19:41:12 +0100

    The fitness app

---
+++ Ch16_Controllers/listing16.03.js
@@ -0,0 +1,17 @@
+// Get Programming with JavaScript
+// Listings 16.02 and 16.03
+// The fitness app
+
+var app = fitnessApp.init(fitnessApp.userData);
+
+
+
+/* Further Adventures
+ *
+ * 1) Create your own user data file
+ *    on JS Bin and import it instead
+ *    of the current data file.
+ *
+ * 2) Log some sessions for your user.
+ *
+ */
