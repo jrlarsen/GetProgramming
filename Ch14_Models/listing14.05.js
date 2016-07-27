067874c31e2b2e048ca1823551d856592af6bd0c
John Larsen <Jr.larsen@mac.com>
2016-07-27 15:26:35 +0100

    A Place constructor with challenges

--- Ch14_Models/listing14.05.js
+++ Ch14_Models/listing14.05.js
@@ -1,56 +1,119 @@
 /* Get Programming with JavaScript
  * Listing 14.05
- * A message view
+ * A Place constructor with challenges
+ * Requires spacer
  */
 
 (function () {
 
+  var Place = function (title, description) {
+    var newLine = spacer.newLine();
+    var items = [];
+    var exits = {};
+    var challenges = {};
+  
+    var getItemsInfo = function () {
+        var itemsString = "Items: " + newLine;
+        items.forEach(function (item) {
+            itemsString += "   - " + item;
+            itemsString += newLine;
+        });
+        return itemsString;
+    };
+  
+    var getExitsInfo = function () {
+        var exitsString = "Exits from " + title;
+        exitsString += ":" + newLine;
+        
+        Object.keys(exits).forEach(function (key) {
+            exitsString += "   - " + key;
+            exitsString += newLine;
+        });
+      
+        return exitsString;
+    };
+
+    var getTitleInfo = function () {
+        return spacer.box(title, title.length + 4, "=");
+    };
+
+    var getInfo = function () {
+        var infoString = getTitleInfo();
+        infoString += description;
+        infoString += newLine + newLine;
+        infoString += getItemsInfo() + newLine;
+        infoString += getExitsInfo();
+        infoString += spacer.line(40, "=") + newLine;
+        return infoString;
+    };
+
+    this.showInfo = function () {
+        console.log(getInfo());
+    };
+
+    this.addItem = function (item) {
+        items.push(item);
+    };
+  
+    this.addExit = function (direction, exit) {
+        exits[direction] = exit;
+    };
+
+    this.getExit = function (direction) {
+        return exits[direction];
+    };
+  
+    this.getLastItem = function () {
+        return items.pop();
+    };
+    
+    this.addChallenge = function (direction, challenge) {
+      challenges[direction] = challenge;
+    };
+  
+    this.getChallenge = function (direction) {
+      return challenges[direction];
+    };
+  };
+  
   if (window.theCrypt === undefined) {
     window.theCrypt = {};
   }
-
-  theCrypt.messageView = (function () {
-    var renderMessage = function (message) {
-      return "*** " + message + " ***";
-    };
   
-    var render = function (message) {
-      console.log(renderMessage(message));
-    };
-  
-    return {
-      render: render
-    };
-  })();
+  theCrypt.Place = Place;  
   
 })();
-
+  
 
 
 /* Further Adventures
  *
  * 1) Run the program.
  *
- * 2) Create a shortcut variable
- *    for the message view.
+ * 2) Create a place.
  *
- *    > var mv = theCrypt.messageView
+ *    > var p = new theCrypt.Place("The Farm", "You are at a farm.")
+ *    > p.showInfo()
  *
- * 3) Display some messages.
+ * 3) Add an exit.
  *
- *    > mv.render("Hello")
- *    > mv.render("wassup wassup wassup")
+ *    > p.addExit("east", "The Farmhouse")
+ *    > p.showInfo()
  *
- * 4) Change the program to display
- *    messages like this:
+ * 4) Create a challenge object.
  *
- *    > mv.render("Hello")
- *      The message is: Hello
+ *    > var ch = {}
+ *    > ch.message = "There is a bull blocking the gate"
+ *    > ch.success = "The bull runs off after the kite"
+ *    > ch.failure = "It's getting angrier"
+ *    > ch.requires = "a red kite"
+ *    > ch.damage = 30
+ *    > ch.itemRequired = true
+ *    > ch
  *
- * 5) Change the program to display
- *    messages in upper case.
+ * 5) Add the challenge.
  *
- *    > mv.render("Hello")
- *      HELLO
+ *    > p.addChallenge("east", ch)
+ *    > p.getChallenge("east")
  *
  */
