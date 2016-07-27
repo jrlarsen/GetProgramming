8311fc798d445568a6f94eca64b0d0b226dd7d90
John Larsen <Jr.larsen@mac.com>
2016-07-27 15:45:30 +0100

    Starting The Sparrow

---
+++ Ch14_Models/TheSparrow.js
@@ -0,0 +1,17 @@
+/* Get Programming with JavaScript
+ * Chapter 14
+ * The Sparrow
+ */
+
+var game = theCrypt.getGame();
+
+
+
+/* Further Adventures
+ *
+ * 1) Play the game, Using the public methods:
+ *
+ *    > game.get()
+ *    > game.go("up")
+ *
+ */
