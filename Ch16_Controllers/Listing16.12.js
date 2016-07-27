f6709ba1dd2fa5b23ce5f26c60aa49639c357cf7
John Larsen <Jr.larsen@mac.com>
2016-07-27 20:12:09 +0100

    Running the game

---
+++ Ch16_Controllers/Listing16.12.js
@@ -0,0 +1,19 @@
+/* Get Programming with JavaScript
+ * Listing 16.12
+ * Running the game
+ */
+
+var playerName = "Jahver";
+var map = theCrypt.mapData;
+
+game.init(map, playerName);
+
+
+
+/* Further Adventures
+ *
+ * 1) Run the program.
+ *
+ * 2) Play the game.
+ *
+ */
