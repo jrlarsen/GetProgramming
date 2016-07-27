d4b3f990b8092d41fc2e287aa0878560a724abe3
John Larsen <Jr.larsen@mac.com>
2016-07-27 21:00:33 +0100

    A web-based The Crypt game

---
+++ Ch17_HTML/listing17.09.js
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>The Crypt</title>  
+</head>
+<body>
+
+  <h1>The Crypt</h1>
+  
+  <div id="place"></div>
+  
+  <div id="player"></div>
+  
+
+  
+  <!-- Modules -->
+  
+  <!-- spacer -->
+  <script src="http://output.jsbin.com/juneqo.js"></script>
+
+
+  <!-- Place constructor -->
+  <script src="http://output.jsbin.com/vuwave.js"></script>
+
+  <!-- Player constructor -->
+  <script src="http://output.jsbin.com/nonari.js"></script>
+
+
+  <!-- player view -->
+  <script src="http://output.jsbin.com/cehexi.js"></script>
+
+  <!-- place view -->
+  <script src="http://output.jsbin.com/cakine.js"></script>
+
+  <!-- message view -->
+  <script src="http://output.jsbin.com/jatofe.js"></script>
+
+
+  <!-- map data -->
+  <script src="http://output.jsbin.com/hozefe.js"></script>
+
+  <!-- map builder -->
+  <script src="http://output.jsbin.com/paqihi.js"></script>
+
+
+  <!-- game controller -->
+  <script src="http://output.jsbin.com/yeqicu.js"></script>
+  
+</body>
+</html>
