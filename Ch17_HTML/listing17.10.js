69dae9fb5a7138ed1022530b64637f35a938ca61
John Larsen <Jr.larsen@mac.com>
2016-07-27 21:10:53 +0100

    A web-based The Crypt game

---
+++ Ch17_HTML/listing17.10.js
@@ -0,0 +1,26 @@
+// Get Programming with JavaScript
+// Listings 17.09 and 17.10
+// A web-based The Crypt game
+
+var playerName = "Jahver";
+var map = theCrypt.mapData;
+
+game.init(map, playerName);
+
+
+/* Further Adventures
+ *
+ * 1) Try playing the game.
+ *    Even though the display isn't great,
+ *    the game should still work.
+ *
+ *    Enter commands at the console prompt.
+ *
+ *    > game.get()
+ *    > game.go("west")
+ *    > game.use("holy water", "south")
+ *
+ *    The output updates and messages
+ *    appear on the console.
+ *
+ */
