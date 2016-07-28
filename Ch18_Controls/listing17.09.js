17fd1b47c4851d06b483814fc199d6b5dff7ba7b
John Larsen <Jr.larsen@mac.com>
2016-07-28 10:31:13 +0100

    The command module

--- Ch18_Controls/listing17.09.js
+++ Ch18_Controls/listing17.09.js
@@ -1,27 +1,57 @@
 /* Get Programming with JavaScript
- * Listing 17.09
- * A web based message view
+ * Listing 18.09
+ * The command module
  */
 
 (function () {
+  "use strict";
+  
+  function parseCommand (commandString) {
+    var commandWords = commandString.split(" ");
+    var command = {
+        type: commandWords.shift()
+    };
 
-  if (window.theCrypt === undefined) {
-    window.theCrypt = {};
+    if (command.type === "go" || command.type === "use") {
+        command.direction = commandWords.pop();
+    }
+
+    command.item = commandWords.join(" ");
+
+    return command;
   }
-
-  theCrypt.messageView = (function () {
-    var renderMessage = function (message) {
-      return "*** " + message + " ***";
-    };
   
-    var render = function (message) {
-      var messageDiv = document.getElementById("messages");
-      messageDiv.innerHTML = renderMessage(message);
-    };
+  function doAction () {
+    var txtCommand = document.getElementById("txtCommand");
+    var commandString = txtCommand.value;
+    var command = parseCommand(commandString);
+    
+    theCrypt.messageView.clear();                                     
   
-    return {
-      render: render
-    };
-  })();
-
+    switch (command.type) {
+      
+      case "get":
+        game.get();
+        break;
+      
+      case "go":
+        game.go(command.direction);
+        break;
+      
+      case "use":
+        game.use(command.item, command.direction);
+        break;
+      
+      default:
+        theCrypt.messageView.render("I don't know how to do that");
+      
+    }
+    
+    txtCommand.value = "";
+    txtCommand.focus();
+  }
+  
+  var commandButton = document.getElementById("btnCommand");
+  commandButton.addEventListener("click", doAction);
+  
 })();
