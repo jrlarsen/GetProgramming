44a37ae496d8a50aad9e1f7a19a348c417239bcd
John Larsen <Jr.larsen@mac.com>
2016-07-28 19:04:25 +0100

    The fitness app

---
+++ Ch20_XHR/listing20.06.js
@@ -0,0 +1,25 @@
+// Get Programming with JavaScript
+// Listings 20.05 and 20.06
+// The fitness app
+
+var app = fitnessApp.init("qiwizo");
+
+// pobapa - Obinna
+// qapavi - Liang
+// qiwizo - Mahesha
+
+
+/* Further Adventures
+ *
+ * 1) Run the program.
+ *
+ * 2) Log some sessions:
+ *
+ *    > app.log("2017-02-07", 20)
+ *
+ * 3) Load another user:
+ *
+ *    > app.loadUser("pobapa")
+ *    > app.log("2017-02-10", 45)
+ *
+ */
